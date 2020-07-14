65e0c62d8583ae17dcdc31b1a86389bf5caaa626
Ria Vora <riavora11@gmail.com>
2020-07-14 12:20:12 -0700

    added user persistence!

--- Represent/SceneDelegate.m
+++ Represent/SceneDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "User.h"
 
 @interface SceneDelegate ()
 
@@ -16,9 +17,10 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    if (User.currentUser) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+    }
 }
 
 
