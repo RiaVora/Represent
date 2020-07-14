68525728f4bfe6e4239e21252c845b7fee1dcfdc
Ria Vora <riavora11@gmail.com>
2020-07-14 09:56:21 -0700

    created a registration screen with fields and alerts to check that all fields have been filled in!

--- Represent/View Controllers/RegistrationViewController.m
+++ Represent/View Controllers/RegistrationViewController.m
@@ -7,9 +7,17 @@
 //
 
 #import "RegistrationViewController.h"
+#import "Utils.h"
+#import "User.h"
 
 @interface RegistrationViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *firstNameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *zipcodeField;
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordField;
 @end
 
 @implementation RegistrationViewController
@@ -21,14 +29,37 @@
     // Do any additional setup after loading the view.
 }
 
-/*
+#pragma mark - Actions
+
+- (IBAction)pressedSignUp:(id)sender {
+    NSString *firstName = self.firstNameField.text;
+    NSString *email = self.emailField.text;
+    NSString *zipcode = self.zipcodeField.text;
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    NSString *confirmPassword = self.confirmPasswordField.text;
+    BOOL firstNameExists = [Utils checkExists:firstName :@"First Name" :self];
+    BOOL emailExists = [Utils checkExists:email :@"Email" :self];
+    BOOL zipcodeExists = [Utils checkExists:zipcode :@"Zipcode" :self];
+    BOOL usernameExists = [Utils checkExists:username :@"Username" :self];
+    BOOL passwordExists = [Utils checkExists:password :@"Password" :self];
+    BOOL confirmPasswordExists = [Utils checkExists:confirmPassword :@"Confirm Password" :self];
+    BOOL allExist = firstNameExists && emailExists && zipcodeExists && usernameExists && passwordExists && confirmPasswordExists;
+    
+    if (allExist) {
+        
+    }
+    
+}
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"signedInSegue"]) {
+        
+    }
+
 }
-*/
+
 
 @end
