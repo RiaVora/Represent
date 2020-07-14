d8fa579b85d0288c0e6c04f66681a5d11c3cba8a
Ria Vora <riavora11@gmail.com>
2020-07-14 08:50:53 -0700

    set up login call and alerts for login!

--- Represent/View Controllers/LoginViewController.m
+++ Represent/View Controllers/LoginViewController.m
@@ -9,8 +9,11 @@
 #import "LoginViewController.h"
 #import "Question.h"
 #import <Parse/Parse.h>
+#import "Utils.h"
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -19,29 +22,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [Question postUserQuestion:@"test question" forRepresentative:nil withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
-        if (succeeded) {
-            NSLog(@"Question successfully saved!");
-        } else {
-            NSLog(@"Unable to save question: %@", error.localizedDescription);
-        }
-    }];
-    PFQuery *questionQuery = [Question query];
-    [questionQuery orderByDescending:@"createdAt"];
-    [questionQuery includeKey:@"author"];
-    questionQuery.limit = 20;
+//    [Question postUserQuestion:@"test question" forRepresentative:nil withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+//        if (succeeded) {
+//            NSLog(@"Question successfully saved!");
+//        } else {
+//            NSLog(@"Unable to save question: %@", error.localizedDescription);
+//        }
+//    }];
+//    PFQuery *questionQuery = [Question query];
+//    [questionQuery orderByDescending:@"createdAt"];
+//    [questionQuery includeKey:@"author"];
+//    questionQuery.limit = 20;
+//
+//    [questionQuery findObjectsInBackgroundWithBlock:^(NSArray<Question *> * _Nullable questions, NSError * _Nullable error) {
+//        if (questions) {
+//            NSLog(@"Successfully received questions!");
+//            for (Question *question in questions) {
+//                NSLog(@"this questions asks %@", question.text);
+//            }
+//        } else {
+//            NSLog(@"There was a problem fetching Questions: %@", error.localizedDescription);
+//        }
+//    }];
     
-    [questionQuery findObjectsInBackgroundWithBlock:^(NSArray<Question *> * _Nullable questions, NSError * _Nullable error) {
-        if (questions) {
-            NSLog(@"Successfully received questions!");
-            for (Question *question in questions) {
-                NSLog(@"this questions asks %@", question.text);
+}
+- (IBAction)pressedLogin:(id)sender {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    BOOL usernameExists = [Utils checkExists:username :@"Username" :self];
+    BOOL passwordExists = [Utils checkExists:password :@"Username" :self];
+    
+    if (usernameExists && passwordExists) {
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %ld", error.code);
+                [Utils displayAlertWithOk:@"Error with Logging in" :error.localizedDescription :self];
+            } else {
+                NSLog(@"User %@ logged in successfully", username);
+                self.usernameField.text = @"";
+                self.passwordField.text = @"";
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
             }
-        } else {
-            NSLog(@"There was a problem fetching Questions: %@", error.localizedDescription);
-        }
-    }];
-    
+        }];
+    }
+}
+- (IBAction)pressedSignUp:(id)sender {
+    [self performSegueWithIdentifier:@"signUpSegue" sender:sender];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"loginSegue"]) {
+        
+    } else if ([segue.identifier isEqualToString: @"signUpSegue"]) {
+        
+    }
 }
 
 
