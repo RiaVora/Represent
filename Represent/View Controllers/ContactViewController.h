4a5be3a626980cda30391230b79084baadc37b28
Ria Vora <riavora11@gmail.com>
2020-07-30 15:10:33 -0700

    created a ContactViewController with a dropdown menu of representatives!

---
+++ Represent/View Controllers/ContactViewController.h
@@ -0,0 +1,20 @@
+//
+//  ContactViewController.h
+//  Represent
+//
+//  Created by Ria Vora on 7/30/20.
+//  Copyright © 2020 Ria Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "User.h"
+#import "RepresentativeCell.h"
+#import "WebKit/WebKit.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Represent/View Controllers/ContactViewController.m
+++ Represent/View Controllers/ContactViewController.m
@@ -1,30 +1,31 @@
 //
-//  PostQuestionsViewController.m
+//  ContactViewController.m
 //  Represent
 //
-//  Created by Ria Vora on 7/16/20.
+//  Created by Ria Vora on 7/30/20.
 //  Copyright © 2020 Ria Vora. All rights reserved.
 //
 
-#import "PostQuestionsViewController.h"
+#import "ContactViewController.h"
 
-@interface PostQuestionsViewController () <UITableViewDelegate, UITableViewDataSource, UITextViewDelegate>
-@property (weak, nonatomic) IBOutlet UITextView *questionField;
+@interface ContactViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UIButton *representativeButton;
 @property (weak, nonatomic) IBOutlet UITableView *tableViewRepresentatives;
-@property (strong, nonatomic) NSArray *representatives;
+@property NSArray *representatives;
+@property User *currentRepresentative;
+
 
 @end
 
-@implementation PostQuestionsViewController
+@implementation ContactViewController
 
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupTableView];
-    self.questionField.delegate = self;
     [self setupValues];
+
 }
 
 #pragma mark - Setup
@@ -37,58 +38,34 @@
 }
 
 - (void)setupValues {
-    self.questionField.text = @"What do you want to ask?";
-    self.questionField.textColor = UIColor.lightGrayColor;
     self.representatives = [User currentUser].followedRepresentatives;
-    [self.currentRepresentative fetch];
-    [self.representativeButton setTitle:[self.currentRepresentative fullTitleRepresentative] forState:UIControlStateNormal];
-}
+    self.currentRepresentative = self.representatives[0];
+    [self.currentRepresentative fetchIfNeededInBackgroundWithBlock:^(PFObject * _Nullable rep, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error with fetching representative to show in dropdown menu: %@", error.localizedDescription);
+        } else {
+            [self.representativeButton setTitle:[self.currentRepresentative fullTitleRepresentative] forState:UIControlStateNormal];
+        }
+    }];
 
-#pragma mark - UITextViewDelegate
-
-- (BOOL)textViewShouldBeginEditing:(UITextView *)textView {
-    if ([textView.textColor isEqual: UIColor.lightGrayColor]) {
-        textView.text = @"";
-        textView.textColor = UIColor.blackColor;
-    }
-    return true;
-}
-
-- (void)textViewDidEndEditing:(UITextView *)textView {
-    if ([textView.text isEqualToString:@""]) {
-        textView.text = @"What do you want to ask?";
-        textView.textColor = UIColor.lightGrayColor;
-    }
 }
 
 #pragma mark - Actions
+
+- (IBAction)pressedRepresentative:(id)sender {
+    self.tableViewRepresentatives.hidden = !(self.tableViewRepresentatives.hidden);
+}
 
 - (IBAction)pressedCancel:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
 
-- (NSString *)pressedPost {
-    NSString *questionText = self.questionField.text;
-    self.questionField.text = @"";
-    BOOL questionExists = [Utils checkExists: questionText:@"Question" :self];
-    if (questionExists) {
-        return questionText;
-    } else {
-        return nil;
-    }
-}
-
-- (IBAction)pressedRepresentative:(id)sender {
-    self.tableViewRepresentatives.hidden = !(self.tableViewRepresentatives.hidden);
-}
-
 #pragma mark - UITableViewDataSource
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     RepresentativeCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RepresentativeCell"];
     User *representative = self.representatives[indexPath.row];
-    [representative fetch];
     if (cell == nil) {
         cell = [[RepresentativeCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"RepresentativeCell"];
     }
@@ -101,11 +78,24 @@
     return self.representatives.count;
 }
 
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     RepresentativeCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     [self.representativeButton setTitle:[cell.representative fullTitleRepresentative] forState:UIControlStateNormal];
     self.currentRepresentative = cell.representative;
     tableView.hidden = YES;
 }
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
